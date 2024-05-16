e2bc087957d2b2999fcf24948d8416105c6e433e
benwzj <57896457+benwzj@users.noreply.github.com>
2024-05-16 11:44:02 +1000

    change icon

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,16 +1,27 @@
-import clsx from 'clsx';
-
 export default function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg
+      width="800px"
+      height="800px"
+      viewBox="0 -0.04 20.088 20.088"
       xmlns="http://www.w3.org/2000/svg"
-      aria-label={`${process.env.SITE_NAME} logo`}
-      viewBox="0 0 32 28"
-      {...props}
-      className={clsx('h-4 w-4 fill-black dark:fill-white', props.className)}
     >
-      <path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z" />
-      <path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z" />
+      <g id="rain" transform="translate(-1.912 -1.986)">
+        <path
+          id="secondary"
+          fill="#2ca9bc"
+          d="M21,9a4,4,0,0,1-4,4H6A3,3,0,1,1,7.08,7.21a5,5,0,0,1,9-2.09A4.08,4.08,0,0,1,17,5a4,4,0,0,1,4,4Z"
+        />
+        <path
+          id="primary"
+          d="M6,17v2m4-2v4m4-4v2m4-2v4M17,5a4.08,4.08,0,0,0-.93.12,5,5,0,0,0-9,2.09A3,3,0,1,0,6,13H17a4,4,0,0,0,0-8Z"
+          fill="none"
+          stroke="#000000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        />
+      </g>
     </svg>
   );
 }
