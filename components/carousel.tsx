72e43a2d6b97a25b6454dd65b798a4e9c0c536b8
benwzj <57896457+benwzj@users.noreply.github.com>
2024-05-16 20:34:27 +1000

    homepage css

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -17,7 +17,7 @@ export async function Carousel() {
         {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
-            className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
+            className="relative aspect-square h-[20vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/6"
           >
             <Link href={`/product/${product.handle}`} className="relative h-full w-full">
               <GridTileImage
--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -14,7 +14,7 @@ function ThreeItemGridItem({
 }) {
   return (
     <div
-      className={size === 'full' ? 'md:col-span-4 md:row-span-2' : 'md:col-span-2 md:row-span-1'}
+      className={size === 'full' ? 'md:col-span-2 md:row-span-1' : 'md:col-span-2 md:row-span-1'}
     >
       <Link className="relative block aspect-square h-full w-full" href={`/product/${item.handle}`}>
         <GridTileImage
@@ -48,7 +48,7 @@ export async function ThreeItemGrid() {
   const [firstProduct, secondProduct, thirdProduct] = homepageItems;
 
   return (
-    <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
+    <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-1">
       <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
       <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
       <ThreeItemGridItem size="half" item={thirdProduct} />
